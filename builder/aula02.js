72bb9837191bf8d18b86d90ca057203c2f7dd038
José-Guilherme-20 <jgscosta20@gmail.com>
2023-12-02 22:03:18 -0300

    chore: iniciando o curso

---
+++ builder/aula02.js
@@ -0,0 +1,5 @@
+"use strict";
+let nome = "José";
+let idade = 20;
+console.log(nome);
+console.log(idade);
---
+++ src/aula02.ts
@@ -0,0 +1,5 @@
+let nome = "José";
+let idade = 20;
+
+console.log(nome);
+console.log(idade);
